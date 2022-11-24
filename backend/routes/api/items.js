5a716b94dc26fc1777507bf4c79cab7c8a337689
Emanuele Moccia <118529155+kubenblinsk@users.noreply.github.com>
2022-11-24 20:50:32 +0100

    Update items.js

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -53,6 +53,10 @@ router.get("/", auth.optional, function(req, res, next) {
     query.tagList = { $in: [req.query.tag] };
   }
 
+  if(typeof req.query.title !== "undefined") {
+    query.title = { $regex: req.query.title };
+  }
+
   Promise.all([
     req.query.seller ? User.findOne({ username: req.query.seller }) : null,
     req.query.favorited ? User.findOne({ username: req.query.favorited }) : null
@@ -70,7 +74,7 @@ router.get("/", auth.optional, function(req, res, next) {
       } else if (req.query.favorited) {
         query._id = { $in: [] };
       }
-
+      console.table(query);
       return Promise.all([
         Item.find(query)
           .limit(Number(limit))
